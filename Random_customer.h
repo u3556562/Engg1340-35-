952a93315bdb645eee14cf5c31f1e181096c8e86
rickychak <47154254+rickychak@users.noreply.github.com>
2019-04-24 02:52:16 +0800

    Update Random_customer.h

--- Random_customer.h
+++ Random_customer.h
@@ -22,9 +22,13 @@ void random50customers(){
 
   for (int i=0; i<custumerno; i++){
     for (int j=0; j<numbergrocery[i]; j++){
-      int whatgrocery = rand()%12;
+      //int whatgrocery = rand()%12;
+      unsigned seed = chrono::steady_clock::now().time_since_epoch().count();
+      uniform_int_distribution<int> uniform_dist2(0,11);
+      default_random_engine pb1(seed);
+      int whatgrocery = uniform_dist2(pb1);
       randomgroceryprice();
-
+      //cout << "whatgrocery= " << whatgrocery <<endl;
       customerlist[i].totalprice+=arr[whatgrocery].random;
 
     }
