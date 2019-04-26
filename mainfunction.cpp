6b81bf155179d336c6b9dbf7fc4656b849a40b89
rickychak <47154254+rickychak@users.noreply.github.com>
2019-04-27 04:59:25 +0800

    Update mainfunction.cpp

--- mainfunction.cpp
+++ mainfunction.cpp
@@ -1,9 +1,50 @@
 #include <iostream>
 #include "Random_customer.h"
 #include "sort.h"
+
 using namespace std;
 
+
+
+
+//              I Strongly suggest you to download all the files and run the program, I guess you will understand what I'm trying to archieve
+
 int main(){
+  char response='Y';
+  int day=1;
+  while (true){
+
+  cout << "Day " << day << ": " << endl;
+  cout << "How many customers today? ";
   random50customers();
   sort();
-}
+
+
+
+  cout << "Go to the next day? (Y/N): ";
+  cin >> response;
+  cout << endl;
+  if (response =='Y'){
+    response = 'Y';
+    day++;
+  }
+  else if (response =='N'){
+    cout << "End of the game." << endl;
+    break;
+  }
+  while (response!= 'Y'){
+    cout << "Unknown Command! Please try again! (Y/N): ";
+    cin >> response;
+    cout << endl;
+    if (response =='N'){
+      cout <<  "End of the game." << endl;
+      return 0;
+    }
+    else if (response == 'Y'){
+      day++;
+    }
+  }
+  }
+
+  return 0;
+  }
