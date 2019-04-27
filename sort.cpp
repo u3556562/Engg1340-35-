e430e5a6051db00ac2ac6340131b9098cb9634a9
Yu Wing Hang <47245703+u3556562@users.noreply.github.com>
2019-04-27 13:46:21 +0800

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -1,25 +1,17 @@
-#ifndef SORT
-#define SORT
-
 #include <iostream>
 #include <algorithm>
 #include <cmath>
 #include "extern.h"
 #include "Random_customer.h"
 using namespace std;
-
+int netincome = 0;
 void sort(){
-	int time[customerno];
-	
+	int *time=new int[customerno];
+
+
 	for (int i=0;i<customerno;i++){
 		time[i]=customerlist[i].totalprice/10;
 	}
-	int sum=0;
-	for (int i=0;i<customerno;i++){
-		sum+=time[i];
-	}
-	cout<<sum<<endl;
-	int cashierno = sum/900-1;
 	int temp1,temp2,temp3;
 	for (int i=0;i<customerno-1;i++){
 		for (int j=0;j<customerno-1-i;j++){
@@ -36,55 +28,84 @@ void sort(){
 			}
 		}
 	}
-	int n=customerno/cashierno+1;
-	customertrolley temp4;
-	customertrolley **sort=new customertrolley*[n];
-	for (int i=0;i<n;i++){
-		sort[i]=new customertrolley[cashierno];
+
+	//row[i]=number of element in cashier i+1 for i = 0 -> column
+	//cashier no = column
+	customertrolley** cashier=new customertrolley* [customerno];
+	for (int i=0;i<customerno;i++){
+		cashier[i]=new customertrolley[customerno];
 	}
-	for (int i=0;i<n;i++){
-		for (int j=0;j<cashierno;j++){
-			if (i*cashierno+j>=customerno){
-				sort[i][j].order=0;
-				sort[i][j].totalprice=0;
-			}
-			else{			
-				sort[i][j]=customerlist[i*cashierno+j];
-			}
+	int *row= new int[customerno];
+	int column=0, count=0, temp=0, n=0, end=0;
+	while (count<customerno){
+		for (int i=0;i<temp;i++){
+			n+=cashier[i][column].totalprice/10+1;
 		}
-		if (i%2==1){
-			for (int k=0;k<cashierno/2;k++){
-				temp4=sort[i][k];
-				sort[i][k]=sort[i][cashierno-k-1];
-				sort[i][cashierno-k]=temp4;
-			}
-		}			
+		if ((n+customerlist[count-end].totalprice/10+1)<900){
+			cashier[temp][column]=customerlist[count-end];
+			temp++, count++;
+		}
+		else{
+			cashier[temp][column]=customerlist[customerno-end-1];
+			row[column]=temp+1;
+			end++, column++, count++;
+			temp=0;
+		}
+		n=0;
 	}
-	for (int i=0;i<n;i++){
-		for (int j=0;j<cashierno;j++){
-			cout<<sort[i][j].totalprice/10<<"	";
+	row[column]=temp;
+	column++;
+	for(int i=0;i<column;i++){
+		for (int j=0;j<row[i];j++){
+			cout<<"i = " << i << "j = " << j <<" "<<cashier[j][i].order<<"	"<<cashier[j][i].totalprice<<"	" << endl;
 		}
-		cout<<endl;
 	}
 	int waitingtime=0;
-	for (int i=0;i<cashierno;i++){
-		cout<<"Cashier "<<i+1<<": "<<endl;
-		for (int j=0;j<n;j++){
+	for(int i=0;i<column;i++){
+		cout<<"Cashier "<<i+1<<":"<<endl;
+		for (int j=row[i]-1;j>=0;j--){
+			for (int k=0;k<j;k++){
+				waitingtime+=cashier[k][i].totalprice/10+1;
+			}
+			cout<<"Customer "<<cashier[j][i].order<<"	"<<cashier[j][i].totalprice<<"	"<<waitingtime<<endl;;
 			waitingtime=0;
-			if (sort[j][i].order>customerno||sort[j][i].order<=0){
-				continue;
-			}
-			else if (j==0){
-				waitingtime=0;
-			}
-			else{
-				for (int k=0;k<j;k++){
-					waitingtime+=sort[k][i].totalprice/10+1;
-				}
-			}
-			cout<<j+1<<":"<<"Customer "<<sort[j][i].order<<": "<<sort[j][i].totalprice<<"	"<<waitingtime<<endl;
 		}
 	}
-}
 
-#endif
+	//cout << column << endl;
+	for (int i=0;i<column;i++){
+		netincome+=customerlist[customerno-1-i].totalprice;
+	}
+
+
+// 				I customised some expenses to make it difficult to save money and open a new cashier line
+	cout <<"NETINCOME = " << netincome << endl;
+	cout << "Cashier Operating Costs = " << column*2500 << endl;
+	cout << "Other Expenses = " << column*1000+500 << endl;
+	netincome = netincome - column*2500 - column*1000 - 500;
+	cout <<"Net Profit = " << netincome<< endl;
+
+
+
+// 				If I can directly change the cashier line number (I'm not sure if column = number of cashier)
+//				And I can't change the column either, so I left a comment there
+	cout << "Do you want to open a new cashier line? Cost = " << column*5000 << " (Y/N): ";
+	char reply;
+	cin >> reply;
+	cout << endl;
+	while (true){
+	if (reply=='Y'){
+		//(no of cashier+1)
+		netincome -= column*5000;
+		break;
+	}
+	else if (reply=='N'){
+		break;
+	}
+	else if (reply!='Y' and reply!='N'){
+		cout << "Unknown Command! " <<endl;
+		cout << "Do you want to open a new cashier line? Cost = " << column*5000 << " (Y?N): ";
+		cin>>reply;
+	}
+	}
+}
